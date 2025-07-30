5d63fc3e339ac76abed51829defae5fde6802093
10017612 <sj.hong@wjthinkbig.com>
2025-07-30 17:00:56 +0900

    30. 누락된 테스트 시나리오 발견

--- src/main/java/commerce/api/controller/SellerSignUpController.java
+++ src/main/java/commerce/api/controller/SellerSignUpController.java
@@ -12,6 +12,7 @@ public record SellerSignUpController() {
     @PostMapping("/seller/signUp")
     ResponseEntity<?> signUp(@RequestBody CreateSellerCommand command) {
         String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
+        String usernameRex = "^[a-z]*$";
         if (command.email() == null) {
             return ResponseEntity.badRequest().build();
         } else if (command.email().contains("@") == false) {
@@ -19,6 +20,16 @@ public record SellerSignUpController() {
         } else if (command.email().endsWith("@")) {
             return ResponseEntity.badRequest().build();
         } else if (command.email().matches(emailRegex) == false) {
+            return ResponseEntity.badRequest().build();
+        } else if (command.username() == null) {
+            return ResponseEntity.badRequest().build();
+        } else if (command.username().isBlank()) {
+            return ResponseEntity.badRequest().build();
+        } else if (command.username().length() < 3) {
+            return ResponseEntity.badRequest().build();
+        } else if (command.username().length() < 3) {
+            return ResponseEntity.badRequest().build();
+        } else if (command.username().matches(usernameRex) == false) {
             return ResponseEntity.badRequest().build();
         } else {
             return ResponseEntity.noContent().build();
