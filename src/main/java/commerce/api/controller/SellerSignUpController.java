ea06905a106a4ba2a75074240ce490fc00046994
10017612 <sj.hong@wjthinkbig.com>
2025-07-30 17:23:59 +0900

    33. 리팩터링

--- src/main/java/commerce/api/controller/SellerSignUpController.java
+++ src/main/java/commerce/api/controller/SellerSignUpController.java
@@ -9,34 +9,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public record SellerSignUpController() {
 
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
+    private static final String USERNAME_REX = "^[a-zA-Z0-9_-]{3,}$";
+
     @PostMapping("/seller/signUp")
     ResponseEntity<?> signUp(@RequestBody CreateSellerCommand command) {
-        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
-        String usernameRex = "^[a-zA-Z0-9_-]*$";
-        if (command.email() == null) {
+        if (isCommandValid(command) == false) {
             return ResponseEntity.badRequest().build();
-        } else if (command.email().contains("@") == false) {
-            return ResponseEntity.badRequest().build();
-        } else if (command.email().endsWith("@")) {
-            return ResponseEntity.badRequest().build();
-        } else if (command.email().matches(emailRegex) == false) {
-            return ResponseEntity.badRequest().build();
-        } else if (command.username() == null) {
-            return ResponseEntity.badRequest().build();
-        } else if (command.username().isBlank()) {
-            return ResponseEntity.badRequest().build();
-        } else if (command.username().length() < 3) {
-            return ResponseEntity.badRequest().build();
-        } else if (command.username().length() < 3) {
-            return ResponseEntity.badRequest().build();
-        } else if (command.username().matches(usernameRex) == false) {
-            return ResponseEntity.badRequest().build();
-        } else if (command.password() == null) {
-            return ResponseEntity.badRequest().build();
-        } else if (command.password().length() < 8) {
-            return ResponseEntity.badRequest().build();
-        } else {
-            return ResponseEntity.noContent().build();
         }
+        
+        return ResponseEntity.noContent().build();
+    }
+
+    private static boolean isCommandValid(CreateSellerCommand command) {
+        return isEmailValid(command.email())
+            && isUsernameValid(command.username())
+            && isPasswordValid(command.password());
+    }
+
+    private static boolean isEmailValid(String email) {
+        return email != null && email.matches(EMAIL_REGEX);
+    }
+
+    private static boolean isUsernameValid(String username) {
+        return username != null && username.matches(USERNAME_REX);
+    }
+
+    private static boolean isPasswordValid(String password) {
+        return password != null && password.length() >= 8;
     }
 }
