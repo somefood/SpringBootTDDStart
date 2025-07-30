0a4b8944cc7d49e767aae34f0dc46a051ae8f100
10017612 <sj.hong@wjthinkbig.com>
2025-07-30 17:13:17 +0900

    32. 관성을 따르는 코드 구현

--- src/main/java/commerce/api/controller/SellerSignUpController.java
+++ src/main/java/commerce/api/controller/SellerSignUpController.java
@@ -31,6 +31,10 @@ public record SellerSignUpController() {
             return ResponseEntity.badRequest().build();
         } else if (command.username().matches(usernameRex) == false) {
             return ResponseEntity.badRequest().build();
+        } else if (command.password() == null) {
+            return ResponseEntity.badRequest().build();
+        } else if (command.password().length() < 8) {
+            return ResponseEntity.badRequest().build();
         } else {
             return ResponseEntity.noContent().build();
         }
