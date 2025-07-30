b642833710459b505d759163c23d3a9af90e62f8
10017612 <sj.hong@wjthinkbig.com>
2025-07-30 17:07:01 +0900

    31. 올바른 입력 데이터 집합

--- src/test/java/test/commerce/api/seller/signup/POST_specs.java
+++ src/test/java/test/commerce/api/seller/signup/POST_specs.java
@@ -142,4 +142,35 @@ public class POST_specs {
         // Assert
         assertThat(response.getStatusCode().value()).isEqualTo(400);
     }
+    
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "seller",
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        "012345678",
+        "seller_",
+        "seller-"
+    })
+    void username_속성이_올바른_형식을_따르면_204_No_Content_상태코드를_반환한다(
+        String username,
+        @Autowired TestRestTemplate client
+    ) {
+        // Arrange
+        var command = new CreateSellerCommand(
+            "seller@test.com",
+            username,
+            "password"
+        );
+        
+        // Act
+        ResponseEntity<Void> response = client.postForEntity(
+            "/seller/signUp",
+            command,
+            Void.class
+        );
+        
+        // Assert
+        assertThat(response.getStatusCode().value()).isEqualTo(204);
+    }
+        
 }
