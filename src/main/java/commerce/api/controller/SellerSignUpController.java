bdbbfa8bb620f1a9e9342aca6b805e7491360f12
somefood <iamseokjuhong@gmail.com>
2025-08-01 00:07:06 +0900

    39. 원래 잘 되던 건데요

--- src/main/java/commerce/api/controller/SellerSignUpController.java
+++ src/main/java/commerce/api/controller/SellerSignUpController.java
@@ -5,12 +5,16 @@ import commerce.SellerRepository;
 import commerce.command.CreateSellerCommand;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public record SellerSignUpController(SellerRepository repository) {
+public record SellerSignUpController(
+    PasswordEncoder passwordEncoder,
+    SellerRepository repository
+) {
 
     private static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
     private static final String USERNAME_REX = "^[a-zA-Z0-9_-]{3,}$";
@@ -21,9 +25,11 @@ public record SellerSignUpController(SellerRepository repository) {
             return ResponseEntity.badRequest().build();
         }
 
+        String hashedPassword = passwordEncoder.encode(command.password());
         var seller = new Seller();
         seller.setEmail(command.email());
         seller.setUsername(command.username());
+        seller.setHashedPassword(hashedPassword);
 
         try {
             repository.save(seller);
