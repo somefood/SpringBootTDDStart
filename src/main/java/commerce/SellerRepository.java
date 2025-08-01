31bd8e14c588aa664dd9fdacab33461744580f79
10017612 <sj.hong@wjthinkbig.com>
2025-08-01 12:01:34 +0900

    43. 테스트 실행 범위

--- src/main/java/commerce/SellerRepository.java
+++ src/main/java/commerce/SellerRepository.java
@@ -2,5 +2,9 @@ package commerce;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SellerRepository extends JpaRepository<Seller, Long> {
+    
+    Optional<Seller> findByEmail(String email);
 }
