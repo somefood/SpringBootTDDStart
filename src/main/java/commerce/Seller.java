84cd375a2e4f1879249c3317916d83cd4b0d42f7
somefood <iamseokjuhong@gmail.com>
2025-08-03 23:04:20 +0900

    55. 엔터티 식별자

--- src/main/java/commerce/Seller.java
+++ src/main/java/commerce/Seller.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Entity
 @Getter
 @Setter
@@ -17,6 +19,9 @@ public class Seller {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long dataKey;
     
+    @Column(unique = true)
+    private UUID id;
+
     @Column(unique = true)
     private String email;
     
