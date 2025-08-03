1ae24683c91fb945909b61e2b3d75d02b4c41165
somefood <iamseokjuhong@gmail.com>
2025-08-03 21:58:01 +0900

    53. HTTP 요청 헤더

--- src/main/java/commerce/api/SecurityConfiguration.java
+++ src/main/java/commerce/api/SecurityConfiguration.java
@@ -33,6 +33,7 @@ public class SecurityConfiguration {
             .authorizeHttpRequests(requests -> requests
                 .requestMatchers("/seller/signUp").permitAll()
                 .requestMatchers("/seller/issueToken").permitAll()
+                .requestMatchers("/seller/me").permitAll()
                 .requestMatchers("/shopper/signUp").permitAll()
                 .requestMatchers("/shopper/issueToken").permitAll()
             )
---
+++ src/main/java/commerce/view/SellerMeView.java
@@ -0,0 +1,6 @@
+package commerce.view;
+
+import java.util.UUID;
+
+public record SellerMeView(UUID id, String email, String username) {
+}
